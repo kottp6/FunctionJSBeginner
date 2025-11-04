fc0cf669a3b64e1bc817cc4afc764885744b1681
Mahmoud Hussein <111658772+kottp6@users.noreply.github.com>
2025-11-04 20:45:18 +0200

    Implement averageOf4Numbers function
    Added a function to calculate the average of four numbers.

--- 2.averageOf4Numbers.js
+++ 2.averageOf4Numbers.js
@@ -1,3 +1,16 @@
+Write a function named averageOf4Numbers that receives 4 numbers as parameters and returns the mathematical average between them.
+
+Example 1
+
+Input
+nr1 = 11
+nr2 = 9
+nr3 = 33
+nr4 = 28
+
+Output
+20.25
+
 function averageOf4Numbers(nr1, nr2, nr3, nr4) {
     return (nr1 + nr2 + nr3 + nr4) / 4;
 }
