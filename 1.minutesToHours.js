77ec9b0d6874e10b2017bc4190307385de473184
Mahmoud Hussein <111658772+kottp6@users.noreply.github.com>
2025-11-04 20:43:39 +0200

    Implement minutesToHours function
    Added a function to convert minutes to hours with an example.

--- 1.minutesToHours.js
+++ 1.minutesToHours.js
@@ -1,3 +1,11 @@
+Write a function named minutesToHours that receives a number of minutes as parameter and returns a number representing the same amount of time but in hours.
+
+Example 1
+Input
+minutes = 150
+Output
+2.5
+
 function minutesToHours(minutes) {
     return minutes / 60;
 }
