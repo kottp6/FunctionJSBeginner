b753544887a6c954ef24a607c2cf6f1b30f79b5f
Mahmoud Hussein <111658772+kottp6@users.noreply.github.com>
2025-11-04 20:49:36 +0200

    Add max5 function to find the largest of five numbers
    Implemented max5 function to return the maximum of five numbers.

---
+++ 4.max5.js
@@ -0,0 +1,17 @@
+Write a function named max5 that receives 5 numbers as parameters and returns the biggest one between them.
+
+Example 1
+Input
+nr1 = 19
+nr2 = 12
+nr3 = 2
+nr4 = -31
+nr5 = 19.5
+Output
+19.5
+
+function max5(nr1, nr2, nr3, nr4, nr5) {
+    return Math.max(nr1, nr2, nr3, nr4, nr5)
+}
+
+export { max5 };
